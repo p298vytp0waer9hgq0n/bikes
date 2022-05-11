c01eb466fa1f6701642d883ce2bcf3d52c5a06c1
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-05-11 18:29:34 +0500

    feat: got it working

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,6 +1,35 @@
-const surfTitleContainer = document.querySelector('.surface__title-container');
-const surfTitles = Array.from(surfTitleContainer.children);
-const surfSlideContainer = document.querySelector('.surface__container');
-const surfSlides = Array.from(surfSlideContainer.children);
-const slideWidth = surfSlides[0].getBoundingClientRect().width;
-console.log(slideWidth);
+// const surfSlides = document.querySelectorAll('.surface__slide');
+// const slideWidth = surfSlides[0].getBoundingClientRect().width;
+const surfaceSection = document.querySelector('.surface');
+const surfaceSlider = document.querySelector('.surface__slider');
+const surfacePrev = document.querySelector('.surface__navigation').firstElementChild;
+const surfaceNext = document.querySelector('.surface__navigation').lastElementChild;
+
+let direction = 0;
+
+surfaceNext.addEventListener('click', function (evt) {
+        // surfaceIndex++;
+        direction = 1;
+        surfaceSlider.style.transform = 'translate(-33.33%)';
+    
+        
+        // surfaceIndex--;
+    });
+
+surfacePrev.addEventListener('click', (evt) => {
+    surfaceSlider.style.transition = 'none'
+    surfaceSlider.prepend(surfaceSlider.lastElementChild);
+    surfaceSlider.style.transform = 'translate(-33.33%)';
+    setTimeout(() => {surfaceSlider.style.transition = 'all .4s'}, 1);
+    setTimeout(() => {surfaceSlider.style.transform = 'translate(0)'}, 1);
+});
+
+surfaceSlider.addEventListener('transitionend', () => {
+        if (direction === 1) {
+            surfaceSlider.style.transition = 'none';
+            surfaceSlider.appendChild(surfaceSlider.firstElementChild);
+            surfaceSlider.style.transform = 'translate(0)';
+            setTimeout(() => {surfaceSlider.style.transition = 'all .4s';}, 1);
+            direction = 0;
+        }
+})
